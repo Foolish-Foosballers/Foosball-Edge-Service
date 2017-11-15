04aafd2ce67df3f246c26c6f6e4ce0e2ccff5095
Daniel Lerner <daniel.lerner2@gmail.com>
2017-11-15 11:33:47 -0500

    piApp

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 
 var amqp = require("amqplib/callback_api");
 var express = require("express");
+var http = require("http");
 var app = express();
+var server = http.createServer(app);
 var config = require("./config.js");
+var socketio = require("socket.io");
+var io = socketio.listen(3080); // need diff port than express app
+
 var url = "amqp://" + config.username + ":" + config.password + "@elephant.rmq.cloudamqp.com/" + config.username
 
-app.get('/receive', function (req, res) {
-    amqp.connect(url, function (err, conn) {
-        conn.createChannel(function (err, ch) {
-          var queue_name = "listen_for_goal";
-          ch.assertQueue(queue_name, { durable: false });
-          ch.consume(queue_name, function (msg) {
-            console.log(msg.content.toString());
-          }, { noAck: true });
-        });
-    });
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/receive.html");
+  amqp.connect(url, function (err, conn) {
+      conn.createChannel(function (err, ch) {
+        var queue_name = "listen_for_goal";
+        ch.assertQueue(queue_name, { durable: false });
+        ch.consume(queue_name, function (msg) {
+          console.log(msg.content.toString());
+          io.sockets.emit("new-item", msg.content.toString());
+        }, { noAck: true });
+      });
+  });
 })
 
 app.listen(3000, function () {
